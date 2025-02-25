e31dd4054a26273c7d5b5b675f27dbc6c06f953d
Peter Pan <94622843+Spidey120703@users.noreply.github.com>
2025-02-26 02:07:27 +0800

    Delete git directory

--- git/git-update-proxy.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-for /f tokens^=2^ usebackq %%i in (`netsh winhttp show advproxy ^| findstr \"ProxyIsEnabled\"`) do @(if "%%i" equ "true," for /f tokens^=4^ delims^=^"^ usebackq %%i in (`netsh winhttp show advproxy ^| findstr \"Proxy\"`) do @(set sockaddr=%%i))
-git config --global http.proxy http://%sockaddr%
-git config --global https.proxy https://%sockaddr%
